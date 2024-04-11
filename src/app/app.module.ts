8c6c1743b4ddefb0566a6a7ef445a495276096fb
Shubham Bahre <shubham.bahre@viklele.com>
2024-04-11 10:53:28 +0530

    Working first run of application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,13 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
-import { TreeDiagramModule } from 'ng-tree-diagram';
+import { AppComponent } from "./app.component";
+import { TreeDiagramModule } from "projects/ng-tree-diagram/src/ng-tree-diagram";
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, TreeDiagramModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule {}
